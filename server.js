e8387bfd89a14be85240054dfe2033b3d51d8ac5
Doctria <skillfinderprojectceutec@gmail.com>
2025-03-29 12:39:03 -0600

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: "https://skillfinderceutec.github.io", // ← tu frontend
+  origin: "https://doctria.github.io", // ← tu frontend
   credentials: true
 }));
 
